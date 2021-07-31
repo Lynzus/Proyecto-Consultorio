5a9d28d593190413c902ccea0fe6e4a81a2ef4c0
Lynzus <79123736+Lynzus@users.noreply.github.com>
2021-07-31 00:40:06 -0400

    Cambio a Contraseña Vacia del Root

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 var conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'Primitivo202101',
+    password:'',
     database:'consultorio'
 });
 
